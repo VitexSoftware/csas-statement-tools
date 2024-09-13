fa41021c417c98e06a9079628745380df99c9dc4
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2024-09-13 13:35:13 +0200

    v0.2.0 release

--- src/raiffeisenbank-balance.php
+++ src/raiffeisenbank-balance.php
@@ -1,10 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * RaiffeisenBank - Balance.
+ * This file is part of the RaiffeisenBank Statement Tools package
  *
- * @author     Vítězslav Dvořák <info@vitexsoftware.com>
- * @copyright  (C) 2023-2024 Spoje.Net
+ * https://github.com/Spoje-NET/pohoda-raiffeisenbank
+ *
+ * (c) Spoje.Net IT s.r.o. <https://spojenet.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace SpojeNet\RaiffeisenBank;
@@ -12,34 +18,37 @@ namespace SpojeNet\RaiffeisenBank;
 use Ease\Shared;
 use VitexSoftware\Raiffeisenbank\ApiClient;
 
-require_once('../vendor/autoload.php');
+require_once '../vendor/autoload.php';
 
 const APP_NAME = 'RaiffeisenBankBalance';
 
 /**
- * Get today's transactons list
+ * Get today's transactons list.
  */
-$options = getopt("o::e::", ['output::environment::']);
-Shared::init(['CERT_FILE', 'CERT_PASS', 'XIBMCLIENTID', 'ACCOUNT_NUMBER'], array_key_exists('environment', $options) ? $options['environment'] : '../.env');
-$destination = array_key_exists('output', $options) ? $options['output'] : \Ease\Shared::cfg('RESULT_FILE', 'php://stdout');
+$options = getopt('o::e::', ['output::environment::']);
+Shared::init(['CERT_FILE', 'CERT_PASS', 'XIBMCLIENTID', 'ACCOUNT_NUMBER'], \array_key_exists('environment', $options) ? $options['environment'] : '../.env');
+$destination = \array_key_exists('output', $options) ? $options['output'] : \Ease\Shared::cfg('RESULT_FILE', 'php://stdout');
 
 ApiClient::checkCertificatePresence(Shared::cfg('CERT_FILE'), true);
 
 $engine = new \Ease\Sand();
+
 if (\Ease\Shared::cfg('APP_DEBUG', false)) {
     $engine->logBanner();
 }
 
 $apiInstance = new \VitexSoftware\Raiffeisenbank\PremiumAPI\GetAccountBalanceApi();
 $xRequestId = time();
+
 try {
     $balance = $apiInstance->getBalance($xRequestId, Shared::cfg('ACCOUNT_NUMBER'));
 } catch (\Exception $e) {
-    echo 'Exception when calling GetAccountBalanceApi->getBalance: ', $e->getMessage(), PHP_EOL;
+    echo 'Exception when calling GetAccountBalanceApi->getBalance: ', $e->getMessage(), \PHP_EOL;
     $balance = ['message' => 'Exception when calling GetAccountBalanceApi->getBalance: ', $e->getMessage()];
 }
 
-$written = file_put_contents($destination, json_encode($balance, \Ease\Shared::cfg('DEBUG') ? JSON_PRETTY_PRINT : 0));
+$written = file_put_contents($destination, json_encode($balance, \Ease\Shared::cfg('DEBUG') ? \JSON_PRETTY_PRINT : 0));
 
 $engine->addStatusMessage(sprintf(_('Saving result to %s'), $destination), $written ? 'success' : 'error');
+
 exit($written ? 0 : 1);
--- src/raiffeisenbank-statement-downloader.php
+++ src/raiffeisenbank-statement-downloader.php
@@ -1,41 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * RaiffeisenBank - Statements downloader.
+ * This file is part of the RaiffeisenBank Statement Tools package
  *
- * @author     Vítězslav Dvořák <info@vitexsoftware.com>
- * @copyright  (C) 2023-2024 Spoje.Net
+ * https://github.com/Spoje-NET/pohoda-raiffeisenbank
+ *
+ * (c) Spoje.Net IT s.r.o. <https://spojenet.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace SpojeNet\RaiffeisenBank;
 
 use Ease\Shared;
-use VitexSoftware\Raiffeisenbank\Statementor;
 use VitexSoftware\Raiffeisenbank\ApiClient;
+use VitexSoftware\Raiffeisenbank\Statementor;
 
-require_once('../vendor/autoload.php');
+require_once '../vendor/autoload.php';
 
-define('APP_NAME', 'RaiffeisenBank Statement Downloader');
+\define('APP_NAME', 'RaiffeisenBank Statement Downloader');
 
-if (array_key_exists(1, $argv) && $argv[1] == '-h') {
+if (\array_key_exists(1, $argv) && $argv[1] === '-h') {
     echo 'raiffeisenbank-statement-downloader [save/to/directory] [format] [path/to/.env]';
     echo "\n";
+
     exit;
 }
 
-Shared::init(['CERT_FILE', 'CERT_PASS', 'XIBMCLIENTID', 'ACCOUNT_NUMBER'], array_key_exists(3, $argv) ? $argv[3] : '../.env');
+Shared::init(['CERT_FILE', 'CERT_PASS', 'XIBMCLIENTID', 'ACCOUNT_NUMBER'], \array_key_exists(3, $argv) ? $argv[3] : '../.env');
 ApiClient::checkCertificatePresence(Shared::cfg('CERT_FILE'), true);
 $engine = new Statementor(Shared::cfg('ACCOUNT_NUMBER'));
+
 if (\Ease\Shared::cfg('APP_DEBUG', false)) {
     $engine->logBanner();
 }
+
 $engine->setScope(Shared::cfg('STATEMENT_IMPORT_SCOPE', 'last_month'));
 $statements = $engine->getStatements(Shared::cfg('ACCOUNT_CURRENCY', 'CZK'), Shared::cfg('STATEMENT_LINE', 'MAIN'));
+
 if (empty($statements) === false) {
     $engine->download(
-        array_key_exists(1, $argv) ? $argv[1] : Shared::cfg('STATEMENTS_DIR', getcwd()),
+        \array_key_exists(1, $argv) ? $argv[1] : Shared::cfg('STATEMENTS_DIR', getcwd()),
         $statements,
-        array_key_exists(2, $argv) ? $argv[2] : Shared::cfg('STATEMENT_FORMAT', 'pdf')
+        \array_key_exists(2, $argv) ? $argv[2] : Shared::cfg('STATEMENT_FORMAT', 'pdf'),
     );
 } else {
     echo "no statements returned\n";
